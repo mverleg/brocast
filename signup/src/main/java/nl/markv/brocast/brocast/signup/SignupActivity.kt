12ab96698a2bc95a06c218d4b8f5256704506e67
mark <mark@elsa>
2018-04-21 15:32:45 +0200

    Search fragment

--- signup/src/main/java/nl/markv/brocast/brocast/signup/SignupActivity.kt
+++ signup/src/main/java/nl/markv/brocast/brocast/signup/SignupActivity.kt
@@ -1,24 +1,20 @@
 package nl.markv.brocast.brocast.signup
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
-import kotlinx.android.synthetic.main.activity_signup.*
-
-class SignupActivity : AppCompatActivity() {
+class SignupActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+//        fab.setOnClickListener { view ->
+//            val intent = Intent(this, AddGroupSelectUsersActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
