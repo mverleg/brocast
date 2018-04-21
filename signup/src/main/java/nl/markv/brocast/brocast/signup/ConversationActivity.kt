5ca1e6dbb506d0587f797c5e7100540e70e98b6b
Sander Kools <skools@keylane.nl>
2018-04-21 19:08:28 +0200

    removed useless stuff

--- signup/src/main/java/nl/markv/brocast/brocast/signup/ConversationActivity.kt
+++ signup/src/main/java/nl/markv/brocast/brocast/signup/ConversationActivity.kt
@@ -3,8 +3,6 @@ package nl.markv.brocast.brocast.signup
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ListView
-import io.github.rockerhieu.emojicon.EmojiconEditText
-import io.github.rockerhieu.emojicon.EmojiconTextView
 
 class ConversationActivity : AppCompatActivity() {
 
