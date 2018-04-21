9f6fd25b251c1f09d816bbdca3c76c32f9a79d0a
Sander Kools <skools@keylane.nl>
2018-04-21 12:29:32 +0200

    initial auth tests

--- signup/src/main/java/nl/markv/brocast/brocast/signup/SignupActivity.kt
+++ signup/src/main/java/nl/markv/brocast/brocast/signup/SignupActivity.kt
@@ -1,14 +1,25 @@
 package nl.markv.brocast.brocast.signup
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
 
 import kotlinx.android.synthetic.main.activity_signup.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
 
 class SignupActivity : AppCompatActivity() {
+
+    var fbAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +30,31 @@ class SignupActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        var btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {view ->
+            signIn(view,"user@company.com", "pass")
+        }
+    }
+
+    fun signIn(view: View, email: String, password: String){
+        showMessage(view,"Authenticating...")
+
+        fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
+            if(task.isSuccessful){
+                var intent = Intent(this, LoggedInActivity::class.java)
+                intent.putExtra("id", fbAuth.currentUser?.email)
+                startActivity(intent)
+
+            }else{
+                showMessage(view,"Error: ${task.exception?.message}")
+            }
+        })
+
+    }
+
+    fun showMessage(view:View, message: String){
+        Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE).setAction("Action", null).show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
