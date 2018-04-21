c42b6f4d5c4187a0956ac025875abb70d8f7c0e7
yhoutzager <yoerihoutzager@gmail.com>
2018-04-21 15:55:08 +0200

    local

--- signup/src/main/java/nl/markv/brocast/brocast/signup/SignedInActivity.kt
+++ signup/src/main/java/nl/markv/brocast/brocast/signup/SignedInActivity.kt
@@ -2,18 +2,33 @@ package nl.markv.brocast.brocast.signup
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_signed_in.*
 
 class SignedInActivity : AppCompatActivity() {
 
+    private var userListView: ListView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signed_in)
 
-        but_sign_out.setOnClickListener({
-            FirebaseAuth.getInstance().signOut()
-            finish()
-        })
+        this.userListView = findViewById(R.id.user_list_view) as ListView
+
+        val userList: UserList = UserList()
+
+        val listItems: arrayList()? = null
+
+        for (i in 0 until UserList.size()) {
+            val user = userList.get(i)
+            listItems[i] = recipe.title
+        }
+
+        val adapter = ArrayAdapter(this, android.R.user_list_view, listItems)
+        userListView.setAdapter(adapter)
+
+
     }
 }
